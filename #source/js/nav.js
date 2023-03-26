f94bf4e25b4b0155a4d0ced0fb4d0546efa2c1b1
DimaWeb44 <dima.web44@gmail.com>
2023-03-26 16:57:04 +0300

    transition page

--- #source/js/nav.js
+++ #source/js/nav.js
@@ -3,3 +3,13 @@ eyeBtn = document.querySelector('.eye-btn')
 eyeBtn && eyeBtn.addEventListener('click', () => {
   document.body.classList.toggle('visually-impaired')
 })
+
+languageBtn = document.querySelectorAll('.language-btn')
+languagePanel = document.querySelectorAll('.language-panel')
+languageBtn.length && languageBtn.forEach(el => {
+  el.addEventListener('click', () => {
+    languagePanel.forEach(el => {
+      el.classList.toggle('language-panel-active')
+    })
+  })
+})
--- js/main.js
+++ js/main.js
@@ -4,26 +4,57 @@ eyeBtn = document.querySelector('.eye-btn')
 eyeBtn && eyeBtn.addEventListener('click', () => {
   document.body.classList.toggle('visually-impaired')
 })
+
+languageBtn = document.querySelectorAll('.language-btn')
+languagePanel = document.querySelectorAll('.language-panel')
+languageBtn.length && languageBtn.forEach(el => {
+  el.addEventListener('click', () => {
+    languagePanel.forEach(el => {
+      el.classList.toggle('language-panel-active')
+    })
+  })
+})
 //_________________________________________________________________________________________________transitionsPage
 
-$('.page').each(function(i,e){
-  $(this).click(function(event){
-    var x = event.pageX;
-    var y = event.pageY;
+$('.page').click(function (event) {
+  var x = event.pageX;
+  var y = event.pageY;
 
-    var nextItem = i + 1;
-    if (nextItem >= $('.page').length){
-      nextItem = 0;
+  $('.page-next').css('z-index', parseInt($('.page').css('z-index')) + 1);
+  $('.page-next').css('clip-path', 'circle(0% at ' + x + 'px ' + y + 'px)');
+
+  anime({
+    targets: $('.page-next'),
+    update: function (anim) {
+      $('.page-next').css('clip-path', 'circle(' + (anim.progress * 2) + '% at ' + x + 'px ' + y + 'px)');
     }
-
-    $('.page:eq('+ nextItem +')').css('z-index', parseInt($(this).css('z-index')) + 1);
-    $('.page:eq('+ nextItem +')').css('clip-path', 'circle(0% at '+ x +'px '+ y +'px)');
-
-    anime({
-      targets: $('.page')[nextItem],
-      update: function(anim) {
-        $('.page:eq('+ nextItem +')').css('clip-path', 'circle('+ (anim.progress*2) +'% at '+ x +'px '+ y +'px)');
-      }
-    });
   });
 });
+
+
+
+$(document).ready(function () {
+  $('.panel-next').click(function () {
+    $('.page').css("display", "none");
+    $('.article-page-1').fadeOut(700,);
+    $('.article-page-2').fadeIn(1400);
+  });
+});
+
+/*
+$(document).ready(function() {
+  $("body").css("display", "none");
+
+  $("body").fadeIn(2000);
+
+  $("a.transition").click(function(event){
+    event.preventDefault();
+    linkLocation = this.href;
+    $("body").fadeOut(1000, redirectPage);
+  });
+
+  function redirectPage() {
+    window.location = linkLocation;
+  }
+});
+*/
