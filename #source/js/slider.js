dc191701536da0a94421bc7b1fdb920be60f9c1c
DimaWeb44 <dima.web44@gmail.com>
2023-03-30 01:50:01 +0300

    add full photo

--- #source/js/slider.js
+++ #source/js/slider.js
@@ -1,7 +1,23 @@
 // _________________________________________________________________________________________ slider.js
-function numberSlide(e) {
+function setNumberSlide(e) {
   let indexSlide = $('.section__slider-2').length && $('.section__slider-2').slick('slickCurrentSlide');
   $('.gallery__slider-big').length && $('.gallery__slider-big').slick('slickGoTo', indexSlide);
 }
 
-$('.section__slider-2').length && $('.section__slider-2').on('beforeChange', () => numberSlide());
+$('.gallery-slider-2').length && $('.gallery-slider-2').on('click', () => setNumberSlide());
+
+
+function setNumberSlideGalleri(e) {
+  let indexSlide = $('.gallery__slider-big').length && $('.gallery__slider-big').slick('slickCurrentSlide');
+  $('.gallery__slider-full-photo').length && $('.gallery__slider-full-photo').slick('slickGoTo', indexSlide);
+}
+
+$('#open-full-photo').length && $('#open-full-photo').on('click', () => setNumberSlideGalleri());
+
+
+function setNumberSlideFullPhoto(e) {
+  let indexSlide = $('.gallery__slider-full-photo').length && $('.gallery__slider-full-photo').slick('slickCurrentSlide');
+  $('.gallery__slider-big').length && $('.gallery__slider-big').slick('slickGoTo', indexSlide);
+}
+
+$('#close-full-photo').length && $('#close-full-photo').on('click', () => setNumberSlideFullPhoto());
