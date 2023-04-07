dd5926ab4c0582d22bb08f39e551841740f81fa1
DimaWeb44 <dima.web44@gmail.com>
2023-04-07 21:59:37 +0300

    fix

--- #source/js/nav.js
+++ #source/js/nav.js
@@ -1,5 +1,4 @@
 // ____________________________________________________________________________________________ nav.js
-
 eyeBtn = document.querySelector('.eye-btn')
 eyeBtn && eyeBtn.addEventListener('click', () => {
   document.body.classList.toggle('visually-impaired')
--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 
-F
 // ____________________________________________________________________________________________ nav.js
-
 eyeBtn = document.querySelector('.eye-btn')
 eyeBtn && eyeBtn.addEventListener('click', () => {
   document.body.classList.toggle('visually-impaired')
